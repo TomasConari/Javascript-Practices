b6b84d63bb842591b5fc19f260ee7523ebf20a2f
Tomas Contreras Laptop <tomasconari@gmail.com>
2024-03-11 21:54:07 -0500

    Added dataTypes and forAndWhile

---
+++ javascriptBasics/dataTypes.js
@@ -0,0 +1,22 @@
+//Data Types
+
+
+    //String
+    const name = "Tomás";
+
+    //Interger
+    const age = 19;
+
+    //Boolean
+    const isAdult = true;
+
+    //Array
+    const exampleArray = ["Tomás", 19, True, [1, 2, 3, 4]];
+
+    //Object
+    const exampleObject = {
+        name:"Tomás",
+        lastname:"Contreras",
+        age:19,
+        isAdult:True
+    };
